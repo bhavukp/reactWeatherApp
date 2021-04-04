1cf2b373fdf80578273c75c0cb8672f4cc9dc0de
bhavuk kumar pamnani <bhavukp@mailf5.com>
2021-04-04 18:26:32 +0530

    Updates

---
+++ precache-manifest.908a54a373a014bc0d7dc604be25c5ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2114c1b85eb4df838deded6c2e258690",
+    "url": "/reactWeatherApp/index.html"
+  },
+  {
+    "revision": "ddcb917acfba83ddd150",
+    "url": "/reactWeatherApp/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "fc44676a7d0d42750318",
+    "url": "/reactWeatherApp/static/css/main.33d63ca0.chunk.css"
+  },
+  {
+    "revision": "ddcb917acfba83ddd150",
+    "url": "/reactWeatherApp/static/js/2.b923649d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactWeatherApp/static/js/2.b923649d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc44676a7d0d42750318",
+    "url": "/reactWeatherApp/static/js/main.32f7a9bb.chunk.js"
+  },
+  {
+    "revision": "dca6cf37c41c3310ac4b",
+    "url": "/reactWeatherApp/static/js/runtime-main.3197b131.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/reactWeatherApp/static/media/bg.b6796c73.jpg"
+  }
+]);
